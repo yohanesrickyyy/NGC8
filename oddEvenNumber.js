f84110e9dffbcbd8eb3ff23b5ec3c444d58e8ffd
yohanesrickyyy <144836740+yohanesrickyyy@users.noreply.github.com>
2024-01-31 12:22:32 +0700

    Add files via upload

---
+++ oddEvenNumber.js
@@ -0,0 +1,35 @@
+// Soal 1
+
+// let i = 1
+// for (let i = 1; i <= 100; i += 3) {
+//   for (let j = i; j < i + 3 && j <= 100; j++) {
+//     if (j % 2 == 0) {
+//       console.log(j + " adalah bilangan genap");
+//     } else {
+//       console.log(j + " adalah bilangan ganjil");
+//     }
+//   }
+// }
+
+//soal 2
+
+// for (let i = 50; i <= 200; i += 3) {
+//   for (let j = i; j < i + 3 && j <= 200; j++) {
+//     if (j % 3 == 0) {
+//       console.log(j + " faktor 3");
+//     } else {
+//       console.log(j + " tidak bisa di bagi 3");
+//     }
+//   }
+// }
+
+// soal 3
+
+for (let i = 100; i <= 200; i += 7) {
+  for (let j = i; j < i + 3 && j <= 200; j++) {
+    if (j % 8 == 0) {
+      console.log(j);
+    } else {
+    }
+  }
+}
