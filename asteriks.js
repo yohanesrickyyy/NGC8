9e61dd8db5c4a1690fe1319463deea7094761d94
yohanesrickyyy <yohanesrickyyy@gmail.com>
2024-01-31 12:56:04 +0700

    upload soal ke 3

---
+++ asteriks.js
@@ -0,0 +1,46 @@
+// soal 1
+
+// let row= 5;
+// for( let i = 1; i <= row; i ++){
+//   let bintang = "";
+//   for ( let j = 1; j <= 1; j++){
+//     bintang += "*"
+//   }
+//   console.log(bintang);
+// }
+
+
+// soal 2
+
+// let row2 = 5;
+// for (let i = 1; i <= row2; i++) {
+//   let bintang = "";
+//   for (let j = 1; j <= 5; j++) {
+//     bintang += "*";
+//   }
+//   console.log(bintang);
+// }
+
+// soal 3
+
+// let row3 = 5;
+// for (let i = 1; i <= row3; i++) {
+//   let bintang = "";
+
+//   for (let j = 1; j <= i; j++) {
+//     bintang += "*";
+//   }
+//   console.log(bintang);
+// }
+
+//soal 4
+
+let row4 = 5;
+for (let i = row4; i >= 1; i--) {
+  let bintang = "";
+
+  for (let j = 1; j <= i; j++) {
+    bintang += "*";
+  }
+  console.log(bintang);
+}
