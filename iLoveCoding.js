eb5d232dc3691d7f2bff01e79c268e2e04af0a1f
yohanesrickyyy <yohanesrickyyy@gmail.com>
2024-01-31 11:43:19 +0700

    Upload soal1

---
+++ iLoveCoding.js
@@ -0,0 +1,21 @@
+let angka = 1;
+for (let angka = 1; angka <= 20; angka++) {
+  console.log(angka + " - I love coding Looping1");
+}
+
+let dua = 20;
+for (let i = dua; i >= 1; i--) {
+  console.log(i + " - I love coding Loopin2");
+}
+
+let w1 = 1;
+while (w1 <= 20) {
+  console.log("looping While 1 " + w1);
+  w1++;
+}
+
+let w2 = 20;
+while (w2 >= 1) {
+  console.log("looping While 1 " + w2);
+  w2--;
+}
